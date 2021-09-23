910a760cba417a72a12533fbf5dd9117c99c3593
Amir <chikchicly@gmail.com>
2021-09-23 18:34:01 +0700

    vue

---
+++ site/js.js
@@ -0,0 +1,22 @@
+const Counter = {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++
+    }, 1000)
+  }
+}
+Vue.createApp(Counter).mount('#counter')
+const TwoWayBinding = {
+  data() {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
+}
+
+Vue.createApp(TwoWayBinding).mount('#two-way-binding')
