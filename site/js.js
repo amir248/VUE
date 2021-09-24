18ae955910a1684084048a2afe1c4adbfe025c31
Amir <chikchicly@gmail.com>
2021-09-24 07:02:09 +0700

    f

--- site/js.js
+++ site/js.js
@@ -1,22 +1,27 @@
-const Counter = {
-  data() {
-    return {
-      counter: 0
-    }
-  },
-  mounted() {
-    setInterval(() => {
-      this.counter++
-    }, 1000)
-  }
-}
-Vue.createApp(Counter).mount('#counter')
-const TwoWayBinding = {
-  data() {
-    return {
-      message: 'Hello Vue!'
-    }
-  }
-}
+// const Counter = {
+//   data() {
+//     return {
+//       counter: 0
+//     }
+//   },
+//   mounted() {
+//     setInterval(() => {
+//       this.counter++
+//     }, 1000)
+//   }
+// }
+// Vue.createApp(Counter).mount('#counter')
+// const TwoWayBinding = {
+//   data() {
+//     return {
+//       message: 'Hello Vue!'
+//     }
+//   }
+// }
+//
+// Vue.createApp(TwoWayBinding).mount('#two-way-binding')
 
-Vue.createApp(TwoWayBinding).mount('#two-way-binding')
+app.component('todo-item', {
+  props: ['todo'],
+  template: `<li>{{ todo.text }}</li>`
+})
