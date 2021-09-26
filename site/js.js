1f98d6d89d44c74c8a88414f17ba25039c7c39a4
Amir <amir.navrutdinov@gmil.com>
2021-09-26 18:45:38 +0700

    lorem

--- site/js.js
+++ site/js.js
@@ -25,7 +25,12 @@
           id:4,
           title:"Amir Shikaren",
           content: "Это новоая крутая фишка писать сайт через фреймворк вью."
-          }
+        },
+        {
+          id:5,
+          title: "Post 5",
+          content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
         ],
         selectedPost: null
       };
@@ -61,7 +66,6 @@
   Vue.component("tab-archive", {
     template: "<div>Тут будет что нибудь написано!</div>"
   });
-
   new Vue({
     el: "#dynamic-component-demo",
     data: {
